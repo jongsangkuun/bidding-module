0107975fa1eba5b6975506215dd9ed44f9d0d2b5
jongsangkuun <43625848+jongsangkuun@users.noreply.github.com>
2025-09-17 16:53:56 +0900

    docs: 테스트 코드에 주석을 추가하여 시나리오 명시
    - 경매 요청부터 종료까지의 과정을 단계별 주석으로 설명

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import bid.room
 
+# 시나리오
+# 1. 유저가 경매 요청을 진행함 -> create_room
+# 2. 딜러가 입찰가 입력 -> place_bid
+# 3. 진행 중인 입찰 정보 모두 조회 -> get_room
+# 4. 입찰 종료 요청 -> close_room
 def main():
     user_id = "test"
+
+    # 1. 유저가 경매 요청을 진행함 -> create_room
     for i in range(5):
         bid.room.create_room(user_id=user_id)
 
@@ -14,15 +21,18 @@ def main():
     test_room_info = bid.room.get_room(room_id=test_room_uid)
     print("test_room_info :::", test_room_info)
 
+    # 2. 딜러가 입찰가 입력 -> place_bid
     test_dealer_id = "test_dealer"
     bid.room.place_bid(room_id=test_room_uid, dealer_id=test_dealer_id, item_price=1000000)
 
+    # 3. 진행 중인 입찰 정보 모두 조회 -> get_room
     print("---------------------")
     for i in bid.room._ROOMS:
         print(bid.room.get_room(room_id=i))
 
     bid.room.close_room(room_id=test_room_uid)
 
+    # 4. 입찰 종료 요청 -> close_room
     print("---------------------")
     for i in bid.room._ROOMS:
         print(bid.room.get_room(room_id=i))
